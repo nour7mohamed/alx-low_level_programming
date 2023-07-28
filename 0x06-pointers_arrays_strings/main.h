12ec9552665f160595ab334a84c60c71cee32290
NourHassanM <nm5644714@gmail.com>
2023-07-27 17:38:05 -0700

    mm

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN.H
- #define MAIN.H
+#define MAIN.H
   
  int _putchar(char); 
  char *_strcat(char *dest, char *src); 
@@ -14,4 +14,4 @@
  void print_number(int n); 
  char *infinite_add(char *n1, char *n2, char *r, int size_r); 
  void print_buffer(char *b, int size); 
- #endif
+#endif
