71466363e905e53211667a3240046366b275ee0c
nour7mohamed <norhanhassan107@gmail.com>
2023-08-19 01:59:44 +0200

    nour

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
-{{nclud main.h{
-//_print_rev_recursion
-//@s:char*S
-//return : void
-void _print_rev_recursion(char *s)
+#include "main.h"
+/**
+ *  * _puts_recursion - prints a string, followed by a new line.
+ *   * @s: string
+ *    * Return: no return.
+ *     */
+void _puts_recursion(char *s)
 {
-	if(*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
