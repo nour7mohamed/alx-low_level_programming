6374f9e531281522eef93e3206b482101ae976af
nour7mohamed <norhanhassan107@gmail.com>
2023-08-19 01:53:36 +0200

    nour

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,16 @@
-#includ main.h{
+{{nclud main.h{
 //_print_rev_recursion
 //@s:char*S
 //return : void
 void _print_rev_recursion(char *s)
 {
+	if(*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
