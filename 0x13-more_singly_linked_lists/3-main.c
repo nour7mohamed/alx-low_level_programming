599e94bdd9aab25b0f0dd5e40b8f375609969ceb
nour7mohamed <norhanhassan107@gmail.com>
2023-08-29 02:32:54 +0300

    mm

--- 0x13-more_singly_linked_lists/3-main.c
+++ 0x13-more_singly_linked_lists/3-main.c
@@ -1,13 +1,11 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ * Return: Always 0.
+ */
 int main(void)
 {
 		listint_t *head;
