af07e6efb8b29ab3146a22d202a469fcd1880a5f
nour7mohamed <norhanhassan107@gmail.com>
2023-08-29 03:11:05 +0300

    mm

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,25 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+ *  * add_nodeint_end -  function that adds a new node at the end of,
+ *   * a listint_t list.
+ *    * @head: pointer to the pointer of head of linked list.
+ *     * @n: data to add to the list.
+ *      *
+ *       * Return: the address of the new element, or NULL if it failed.
+ *        */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *head;
-head = NULL;
-add_nodeint_end(&head, 0);
-add_nodeint_end(&head, 1);
-add_nodeint_end(&head, 2);
-add_nodeint_end(&head, 3);
-add_nodeint_end(&head, 4);
-add_nodeint_end(&head, 98);
-add_nodeint_end(&head, 402);
-add_nodeint_end(&head, 1024);
-print_listint(head);
-return (0);
+		listint_t *new_node;
+			listint_t *traverse = *head; /* pointer to the first node */
+
+				/* create a new node */
+				new_node = malloc(sizeof(listint_t));
+					/* check if malloc has failed and return null */
+					if (new_node == NULL)
+								return (NULL);
+						/* access the n field of the struct and initialize it with n */
+						/* which has been passed to the function */
+						new_node->n = n;
+							/* since we are adding to the end of the list then the */
+							/* next field should point to the end of the current list ie NULL */
+							new_node->next = NULL;
+								/* if the list is empty ie value at head is NULL */
+								if (*head == NULL) /* derefrence to get first value */
+										{
+													*head = new_node; /* address of the new_node */
+															return (new_node);
+																}
+									/* if the list is not empty traverse to the end of the list */
+									while (traverse->next != NULL)
+												traverse = traverse->next;
+											/* loop exits once traverse->next == NULL ie at end of list */
+										/* set the new_node as last node */
+										traverse->next = new_node;
+											return (new_node);
 }
