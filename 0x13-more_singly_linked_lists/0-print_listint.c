f47695a1d923a10f92e39e1f990b1739aea1c984
nour7mohamed <norhanhassan107@gmail.com>
2023-08-29 05:00:24 +0300

    nn

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-		size_t count = 0;
+size_t count = 0;
 
-			/* while we have a pointer to head */
-			while (h != NULL)
-					{
-					  printf("%d\n", h->n);
-					h = h->next;
-					count++;
-					}
-			return (count);
+/* while we have a pointer to head */
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
